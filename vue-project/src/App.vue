8e1079447755c5aa75b18949bc69ab81350894f0
laiza <laizacristina2507@gmail.com>
2026-03-31 00:08:50 -0300

    Lista de tarefas com vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,114 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue"
+
+const novaTarefa = ref("")
+const tarefas = ref([])
+
+function adicionarTarefa(){
+  if(novaTarefa.value.trim() === "") return
+
+  tarefas.value.push({ text: novaTarefa.value, concluida: false
+
+   })
+  novaTarefa.value = ""
+}
+
+function removerTarefa(index){
+  tarefas.value.splice(index, 1)
+}
+function alternarStatus(index){
+  tarefas.value[index].concluida =
+    !tarefas.value[index].concluida
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container">
+    <h1>Lista de tarefas</h1>
+
+    <input 
+      v-model="novaTarefa"
+      placeholder="Digite uma tarefa"
+    />
+
+    <button @click="adicionarTarefa">
+      adicionar
+    </button>
+
+    <ul>
+      <li v-for="(tarefa, index) in tarefas" :key="index">
+
+  <span
+    :class="{ concluida: tarefa.concluida }"
+    @click="alternarStatus(index)"
+  >
+        {{ tarefa.text}}
+</span>
+        <button @click="removerTarefa(index)">
+          remover
+        </button>
+
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+.container{
+  max-width: 420px;
+  margin: 60px auto;
+  padding: 25px;
+  background: white;
+  border-radius: 12px;
+  box-shadow: 0 10px 25px rgba(0,0,0,0.08);
+  font-family: system-ui, sans-serif;
+}
+h1{
+  margin-bottom: 20px;
+  color: #333;
+}
+
+input{
+width: 70%;
+  padding: 10px;
+  border-radius: 8px;
+  border: 1px solid #ddd;
+  outline: none;
+}
+input:focus{
+  border-color: #6366f1;
+}
+
+button{
+   padding: 10px 14px;
+  border-radius: 8px;
+  border: none;
+  background: #6366f1;
+  color: white;
+  cursor: pointer;
+  transition: 0.2s;
+}
+button:hover{
+  background: #4f46e5;
+}
+
+ul{
+  list-style: none;
+  padding: 0;
+  margin-top: 20px;
+}
+li{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #f9fafb;
+  padding: 10px;
+  margin-bottom: 8px;
+  border-radius: 8px;
+}
+.concluida{
+  text-decoration: line-through;
+  color: gray;
+}
+
+</style>
